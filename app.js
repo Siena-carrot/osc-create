2c47917585d82627996e0d91d3961ff7d2682b95
Siena_carrot <sietraine7@gmail.com>
2025-12-24 23:12:36 +0900

    a

--- app.js
+++ app.js
@@ -367,7 +367,7 @@ function showSharePopup(dishes) {
         <div class="share-popup-content">
             <h3>共有</h3>
             <p class="share-note">ぜひ画像で保存して添付してね</p>
-            <button class="share-option-btn share-option-btn-red" id="share-x">Xで共有</button>
+            <button class="share-option-btn share-option-btn-red" id="share-x">Twitterで共有</button>
             <button class="share-option-btn" id="share-copy-link">リンクをコピー</button>
             <button class="share-option-btn" id="share-save-image">画像として保存</button>
             <button class="close-popup-btn" id="close-share-popup">とじる</button>
