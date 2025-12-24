ca19aa62d60bbb3cf9c5095d6fb9d9e917253f00
Siena_carrot <sietraine7@gmail.com>
2025-12-24 23:06:10 +0900

    a

--- app.js
+++ app.js
@@ -210,7 +210,7 @@ function displayDishes(container, dishes, title, isSharedView = false) {
         <div class="gacha-result-popup-content">
             ${!isSharedView ? '<button class="close-result-popup" id="close-result-popup">&times;</button>' : ''}
             <div id="gacha-result-content">
-                <h3 style="margin-bottom: 30px; color: #494949; font-size: 1.5em;">${title}</h3>
+                <h3 style="margin-bottom: 30px; color: #494949; font-size: 1.8em;">${title}</h3>
     `;
     
     dishes.forEach((dish, index) => {
